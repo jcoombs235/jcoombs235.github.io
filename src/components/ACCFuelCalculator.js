9821d5a4063d7bd013a700a898ba23bd3c61bb8f
Jake Coombs <jakecoombs235@gmail.com>
2021-02-05 20:03:54 +0000

    Fuel Calculator Form

--- src/components/ACCFuelCalculator.js
+++ src/components/ACCFuelCalculator.js
@@ -1,8 +1,71 @@
+import { useFormik } from "formik";
 import React from "react";
+
+export const Calculator = () => {
+  const [riskyFuel, setRiskyFuel] = React.useState(0);
+  const [safeFuel, setSafeFuel] = React.useState(0);
+
+  const formik = useFormik({
+    initialValues: {
+      minutes: undefined,
+      seconds: undefined,
+      stint: undefined,
+      litres: undefined,
+    },
+    onSubmit: (values) => {
+      setRiskyFuel(0);
+      setSafeFuel(1);
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label>Lap Time</label>
+      <input
+        id="minutes"
+        name="minutes"
+        type="number"
+        onChange={formik.handleChange}
+        value={formik.values.minutes}
+      />
+      <input
+        id="seconds"
+        name="seconds"
+        type="number"
+        onChange={formik.handleChange}
+        value={formik.values.seconds}
+      />
+      <label>Stint Length</label>
+      <input
+        id="stint"
+        name="stint"
+        type="number"
+        onChange={formik.handleChange}
+        value={formik.values.stint}
+      />
+      <label>Litres Per Lap</label>
+      <input
+        id="litres"
+        name="litres"
+        type="number"
+        onChange={formik.handleChange}
+        value={formik.values.litres}
+      />
+      <h2>Risky Fuel {riskyFuel}L</h2>
+      <h2>Safe Fuel {safeFuel}L</h2>
+      <button type="submit">Calculate</button>
+    </form>
+  );
+};
 
 class ACCFuelCalculator extends React.Component {
   render() {
-    return <h1>ACC Fuel Calculator</h1>;
+    return (
+      <>
+        <h1>ACC Fuel Calculator</h1>
+        <Calculator />
+      </>
+    );
   }
 }
 
--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -9,6 +9,90 @@ const GlobalStyle = createGlobalStyle`
     color: ${theme.textColor};
     font-family: Open-Sans, Helvetica, Sans-Serif;
   }
+  label {
+      font-weight: bold;
+      display: flex;
+    }
+
+    input[type="submit"],
+    input[type="reset"],
+    input[type="button"],
+    button {
+      padding: 0.5rem 1.25rem;
+      font-size: 1rem;
+      border-radius: 10px;
+      background-color: black;
+      border: 2px solid white;
+      color: white;
+      text-decoration: none;
+      font-weight: bold;
+      margin-bottom: 1rem;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+      display: inline-block;
+      line-height: initial;
+      transition: background-color 200ms ease-in-out, border 200ms ease-in-out,
+        transform 200ms ease-in-out;
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      user-select: none;
+    }
+
+    @media (hover: hover) {
+      input[type="submit"]:hover,
+      input[type="reset"]:hover,
+      input[type="button"]:hover,
+      button:hover {
+        cursor: pointer;
+        border-color: red;
+      }
+    }
+
+    button:focus-visible,
+    button:focus,
+    input[type="submit"]:focus-visible,
+    input[type="reset"]:focus-visible,
+    input[type="button"]:focus-visible {
+      border-color: red;
+      outline: none;
+    }
+
+    input[type="email"],
+    input[type="text"],
+    input[type="number"] {
+      padding: 0.65rem 0.5rem;
+      font-size: 1rem;
+      border: 2px solid white;
+      background-color: black;
+      color: white;
+      border-radius: 10px;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+    }
+
+    input[type="email"],
+    input[type="text"],
+    input[type="number"],
+    textarea,
+    select {
+      width: 400px;
+    }
+
+    input:focus,
+    select:focus,
+    textarea:focus {
+      outline: none;
+      border: 2px solid red;
+    }
+
+    input:invalid,
+    select:invalid,
+    textarea:invalid {
+      border: 2px solid #ff7d87;
+      box-shadow: none;
+    }
 `;
 
 export default GlobalStyle;
