4355ae92cbbe79543cad7747a3f5bcfb6f494b15
Jake Coombs <jakecoombs235@gmail.com>
2021-02-10 00:10:25 +0000

    Styled App Link buttons

--- src/components/Calculator/AppLinks.js
+++ src/components/Calculator/AppLinks.js
@@ -1,29 +1,46 @@
 import React from "react";
-import { isAndroid, isIOS, isMobile } from "react-device-detect";
+import styled from "styled-components";
+
+class AppLinks extends React.Component {
+  render() {
+    return (
+      <>
+        <AppButtons>{AndroidLink}</AppButtons>
+        <AppButtons>{IOSLink}</AppButtons>
+      </>
+    );
+  }
+}
+
+const AppButtons = styled.div`
+  display: block;
+  margin: auto;
+`;
+
+const IOSButton = styled.img`
+  height: 60px;
+`;
+
+const AndroidButton = styled.img`
+  height: 80px;
+`;
 
 const AndroidLink = (
   <a href="https://play.google.com/store/apps/details?id=com.accfuelcalc.accfuelcalculator&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
-    <img
+    <AndroidButton
       alt="Get it on Google Play"
       src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
     />
   </a>
 );
 
-function getLinksForDevice() {
-  if (!isMobile) {
-    return AndroidLink;
-  } else if (isAndroid) {
-    return AndroidLink;
-  } else if (isIOS) {
-    return <h2>IOS</h2>;
-  }
-}
-
-class AppLinks extends React.Component {
-  render() {
-    return getLinksForDevice();
-  }
-}
+const IOSLink = (
+  <a href="https://apps.apple.com/us/app/acc-fuel-calculator/id1540153714?itsct=apps_box&amp;itscg=30200">
+    <IOSButton
+      src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-US?size=250x83&amp;releaseDate=1605225600&h=bc35008d192e5529634991e6e8c153c5"
+      alt="Download on the App Store"
+    />
+  </a>
+);
 
 export default AppLinks;
--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -10,90 +10,90 @@ const GlobalStyle = createGlobalStyle`
     font-family: Open-Sans, Helvetica, Sans-Serif;
   }
   label {
-      font-weight: bold;
-      display: flex;
-    }
+    font-weight: bold;
+    display: flex;
+  }
 
-    input[type="submit"],
-    input[type="reset"],
-    input[type="button"],
-    button {
-      padding: 0.5rem 1.25rem;
-      font-size: 1rem;
-      border-radius: 10px;
-      background-color: black;
-      border: 2px solid white;
-      color: white;
-      text-decoration: none;
-      font-weight: bold;
-      margin-bottom: 1rem;
-      -webkit-appearance: none;
-      -moz-appearance: none;
-      appearance: none;
-      display: inline-block;
-      line-height: initial;
-      transition: background-color 200ms ease-in-out, border 200ms ease-in-out,
-        transform 200ms ease-in-out;
-      -webkit-touch-callout: none;
-      -webkit-user-select: none;
-      user-select: none;
-    }
+  input[type="submit"],
+  input[type="reset"],
+  input[type="button"],
+  button {
+    padding: 0.5rem 1.25rem;
+    font-size: 1rem;
+    border-radius: 10px;
+    background-color: black;
+    border: 2px solid white;
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+    margin-bottom: 1rem;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    display: inline-block;
+    line-height: initial;
+    transition: background-color 200ms ease-in-out, border 200ms ease-in-out,
+      transform 200ms ease-in-out;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    user-select: none;
+  }
 
-    @media (hover: hover) {
-      input[type="submit"]:hover,
-      input[type="reset"]:hover,
-      input[type="button"]:hover,
-      button:hover {
-        cursor: pointer;
-        border-color: red;
-      }
-    }
-
-    button:focus-visible,
-    input[type="submit"]:focus-visible,
-    input[type="reset"]:focus-visible,
-    input[type="button"]:focus-visible {
+  @media (hover: hover) {
+    input[type="submit"]:hover,
+    input[type="reset"]:hover,
+    input[type="button"]:hover,
+    button:hover {
+      cursor: pointer;
       border-color: red;
-      outline: none;
     }
+  }
 
-    input[type="email"],
-    input[type="text"],
-    input[type="number"] {
-      padding: 0.65rem 0.5rem;
-      font-size: 1rem;
-      border: 2px solid white;
-      background-color: black;
-      color: white;
-      border-radius: 10px;
-      -webkit-appearance: none;
-      -moz-appearance: none;
-      appearance: none;
-    }
+  button:focus-visible,
+  input[type="submit"]:focus-visible,
+  input[type="reset"]:focus-visible,
+  input[type="button"]:focus-visible {
+    border-color: red;
+    outline: none;
+  }
 
-    input[type="email"],
-    input[type="text"],
-    input[type="number"],
-    textarea,
-    select {
-      width: 200px;
-      margin: 5px 0;
-      margin-right: 10px;
-    }
+  input[type="email"],
+  input[type="text"],
+  input[type="number"] {
+    padding: 0.65rem 0.5rem;
+    font-size: 1rem;
+    border: 2px solid white;
+    background-color: black;
+    color: white;
+    border-radius: 10px;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+  }
 
-    input:focus,
-    select:focus,
-    textarea:focus {
-      outline: none;
-      border: 2px solid red;
-    }
+  input[type="email"],
+  input[type="text"],
+  input[type="number"],
+  textarea,
+  select {
+    width: 150px;
+    margin: 5px 0;
+    margin-right: 10px;
+  }
 
-    input:invalid,
-    select:invalid,
-    textarea:invalid {
-      border: 2px solid #ff7d87;
-      box-shadow: none;
-    }
+  input:focus,
+  select:focus,
+  textarea:focus {
+    outline: none;
+    border: 2px solid red;
+  }
+
+  input:invalid,
+  select:invalid,
+  textarea:invalid {
+    border: 2px solid #ff7d87;
+    box-shadow: none;
+  }
 `;
 
 export default GlobalStyle;
